0a04f8a4bef259b368285e5f3c93d765e089126b
OldAcc <131222639+Waterforces@users.noreply.github.com>
2025-03-16 14:30:22 +0700

    Update BS-Tree.cpp

--- BS-Tree.cpp
+++ BS-Tree.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
 using namespace std;
 typedef struct node {
     int l=-1,r=-1;
 } node;
 node x[10005];
-int n,root;
+int Q,root=-1;
 
 void inorder(int cur) {
     if(x[cur].l!=-1) inorder(x[cur].l);
@@ -12,30 +14,64 @@ void inorder(int cur) {
     if(x[cur].r!=-1) inorder(x[cur].r);
 }
 
+void preorder(int cur) {
+    cout << cur << " ";
+    if(x[cur].l!=-1) preorder(x[cur].l);
+    if(x[cur].r!=-1) preorder(x[cur].r);
+}
+
+void postorder(int cur) {
+    if(x[cur].l!=-1) postorder(x[cur].l);
+    if(x[cur].r!=-1) postorder(x[cur].r);
+    cout << cur << " ";
+}
+
+bool fnBST(int cur,int ans) {
+    if(x[cur].l!=-1) inorder(x[cur].l);
+    if(cur==ans) return 1;
+    if(x[cur].r!=-1) inorder(x[cur].r);
+    return 0;
+}
+
 int main() {
-    cin >> n >> root;
-    for(int i=1;i<n;i++) {
-        int val,cur=root;
-        cin >> val;
-        while(true) {
-            if(val<cur) {
-                if(x[cur].l==-1) {
-                    x[cur].l=val;
-                    break ;
-                }
-                cur=x[cur].l;
-                continue ;
-            }
+    cin >> Q;
+    while(Q--) {
+        int n,cur=root;
+        char s;
+        cin >> s >> n;
+        if(s=='I') {
+            if(root==-1) root=n;
             else {
-                if(x[cur].r==-1) {
-                    x[cur].r=val;
-                    break ;
+                while(true) {
+                    if(n>cur) {
+                        if(x[cur].r==-1) {
+                            x[cur].r=n;
+                            break ;
+                        }
+                        cur=x[cur].r;
+                    }
+                    else {
+                        if(x[cur].l==-1) {
+                            x[cur].l=n;
+                            break ;
+                        }
+                        cur=x[cur].l;
+                    }
                 }
-                cur=x[cur].r;
-                continue ;
             }
         }
+        else if(s=='R') {
+
+        }
+        else if(s=='F') {
+            if(fnBST(root,n)) cout << "Y\n";
+            else cout << "N\n";
+        }
+        else {
+            if(n==1) inorder(root);
+            else if(n==2) preorder(root);
+            else postorder(root);
+            cout << endl;
+        }
     }
-    inorder(root);
-    //BS-Tree
 }
